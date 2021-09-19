8484da52afccc307bed626248d047138603b6e2c
Jef Mitchell <jef@project87.net>
2021-09-19 19:06:51 -0400

    Update SQL - Initial database schema added as well as some seed data.

---
+++ db/seed.sql
@@ -0,0 +1,22 @@
+insert into department_data (id, name) values (1, 'Training');
+insert into department_data (id, name) values (2, 'Support');
+insert into department_data (id, name) values (3, 'Engineering');
+insert into department_data (id, name) values (4, 'Research and Development');
+insert into department_data (id, name) values (5, 'Sales');
+
+insert into role_data (id, title, salary) values (1, 'Supervisor', '$86308.32');
+insert into role_data (id, title, salary) values (2, 'Surveyor', '$70907.65');
+insert into role_data (id, title, salary) values (3, 'Construction Expeditor', '$57735.38');
+insert into role_data (id, title, salary) values (4, 'Construction Manager', '$67344.41');
+insert into role_data (id, title, salary) values (5, 'Subcontractor', '$78348.52');
+
+insert into employee_data (id, first_name, last_name) values (1, 'Casi', 'Fendt');
+insert into employee_data (id, first_name, last_name) values (2, 'Cole', 'Hourigan');
+insert into employee_data (id, first_name, last_name) values (3, 'Marcellus', 'Harce');
+insert into employee_data (id, first_name, last_name) values (4, 'Wheeler', 'Verling');
+insert into employee_data (id, first_name, last_name) values (5, 'Annaliese', 'Roelofs');
+insert into employee_data (id, first_name, last_name) values (6, 'Redford', 'Lehrmann');
+insert into employee_data (id, first_name, last_name) values (7, 'Brade', 'Furse');
+insert into employee_data (id, first_name, last_name) values (8, 'Tito', 'Hoodlass');
+insert into employee_data (id, first_name, last_name) values (9, 'Stanton', 'Pendred');
+insert into employee_data (id, first_name, last_name) values (10, 'Bevin', 'MacNeilage');
