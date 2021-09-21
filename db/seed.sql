dd2bc6cb854bb6a1f631ca32adea0a70fbdb0116
Jef Mitchell <jef@project87.net>
2021-09-21 17:29:55 -0400

    Update SQL - (seed.sql) Fixed SQL syntax for seed data.

--- db/seed.sql
+++ db/seed.sql
@@ -1,22 +1,30 @@
-insert into department_data (id, name) values (1, 'Training');
-insert into department_data (id, name) values (2, 'Support');
-insert into department_data (id, name) values (3, 'Engineering');
-insert into department_data (id, name) values (4, 'Research and Development');
-insert into department_data (id, name) values (5, 'Sales');
+INSERT INTO
+    `department` (id, `name`)
+VALUES
+    (1, 'Training') (2, 'Support'),
+    (3, 'Engineering'),
+    (4, 'Research and Development'),
+    (5, 'Sales');
 
-insert into role_data (id, title, salary) values (1, 'Supervisor', '$86308.32');
-insert into role_data (id, title, salary) values (2, 'Surveyor', '$70907.65');
-insert into role_data (id, title, salary) values (3, 'Construction Expeditor', '$57735.38');
-insert into role_data (id, title, salary) values (4, 'Construction Manager', '$67344.41');
-insert into role_data (id, title, salary) values (5, 'Subcontractor', '$78348.52');
+INSERT INTO
+    `role` (id, title, salary)
+VALUES
+    (1, 'Supervisor', '86308.32'),
+    (2, 'Surveyor', '70907.65'),
+    (3, 'Trainer', '57735.38'),
+    (4, 'Manager', '67344.41'),
+    (5, 'Subcontractor', '78348.52');
 
-insert into employee_data (id, first_name, last_name) values (1, 'Casi', 'Fendt');
-insert into employee_data (id, first_name, last_name) values (2, 'Cole', 'Hourigan');
-insert into employee_data (id, first_name, last_name) values (3, 'Marcellus', 'Harce');
-insert into employee_data (id, first_name, last_name) values (4, 'Wheeler', 'Verling');
-insert into employee_data (id, first_name, last_name) values (5, 'Annaliese', 'Roelofs');
-insert into employee_data (id, first_name, last_name) values (6, 'Redford', 'Lehrmann');
-insert into employee_data (id, first_name, last_name) values (7, 'Brade', 'Furse');
-insert into employee_data (id, first_name, last_name) values (8, 'Tito', 'Hoodlass');
-insert into employee_data (id, first_name, last_name) values (9, 'Stanton', 'Pendred');
-insert into employee_data (id, first_name, last_name) values (10, 'Bevin', 'MacNeilage');
+INSERT INTO
+    `employee` (id, first_name, last_name)
+VALUES
+    (1, 'Casi', 'Fendt'),
+    (2, 'Cole', 'Hourigan'),
+    (3, 'Marcellus', 'Harce'),
+    (4, 'Wheeler', 'Verling'),
+    (5, 'Annaliese', 'Roelofs'),
+    (6, 'Redford', 'Lehrmann'),
+    (7, 'Brade', 'Furse'),
+    (8, 'Tito', 'Hoodlass'),
+    (9, 'Stanton', 'Pendred'),
+    (10, 'Bevin', 'MacNeilage');
